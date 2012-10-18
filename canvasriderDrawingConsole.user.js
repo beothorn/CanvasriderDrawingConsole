8a516f83882acacf77c9a497f33657f8f1fbb86c
Lucas <lucass@gmail.com>
2012-10-18 00:23:49 -0300

    ReferenceError: repeattimeout is not defined

--- canvasriderDrawingConsole.user.js
+++ canvasriderDrawingConsole.user.js
@@ -60,12 +60,12 @@ input.setAttribute('id','console');
 input.setAttribute('rows','5');
 div.appendChild(input);
 
-setTimeout('repeattimeout()', 100);
-
 function repeattimeout() {
     document.getElementById('coords').value = 'x: '+AF.x+' y: '+AF.y;
     setTimeout('repeattimeout()', 100);
 }
+
+setTimeout('repeattimeout()', 100);
 
 document.getElementById('console').value = 'var x1 = 140; var y1 = -50; var x2 = 350; var y2 = -50; addLine(x1,y1,x2,y2); curveUp(40,50,100,1); curveUp(350,-50,1000,20); curveDown(1350,-50,1000,20); curveUp(2350,950,1000,20);';
 
