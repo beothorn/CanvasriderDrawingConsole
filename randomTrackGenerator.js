a27db9d73a1379e7079f57e636cbefb2ed25718f
Lucas <lucass@gmail.com>
2012-10-22 23:40:35 -0200

    Tweaked random track generator

--- randomTrackGenerator.js
+++ randomTrackGenerator.js
@@ -1,4 +1,5 @@
-for(var i=0;i<100;i++){
+writeText("Randomly generated track",-2,-365);
+for(var i=0;i<20;i++){
     var randSize = Math.floor((Math.random()*1000)+300);
     var depth = randSize / Math.floor((Math.random()*8)+2);
     if(i%2==0){
